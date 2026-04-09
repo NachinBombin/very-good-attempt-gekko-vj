eca36fc28500ad8b1bd4040be9e94185b5c060db
NachinBombin <solidnachox@gmail.com>
2026-04-08 23:58:44 -0400

    feat(nikita): add self-contained sent_gekko_nikita entity + wire init.lua

---
+++ entities/entities/sent_gekko_nikita/shared.lua
@@ -0,0 +1,9 @@
+-- ============================================================
+--  sent_gekko_nikita / shared.lua
+-- ============================================================
+ENT.Type         = "anim"
+ENT.Base         = "base_anim"
+ENT.PrintName    = "Gekko Nikita Missile"
+ENT.Author       = "GekkoNPC"
+ENT.Spawnable    = false
+ENT.AdminSpawnable = false
