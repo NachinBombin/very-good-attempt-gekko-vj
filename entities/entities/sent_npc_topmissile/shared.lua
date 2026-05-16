43929b4053e893af58cce4eacb12d09c744e6020
NachinBombin <solidnachox@gmail.com>
2026-05-16 01:36:34 -0400

    Delete entities directory

--- entities/entities/sent_npc_topmissile/shared.lua
+++
@@ -1,8 +0,0 @@
-AddCSLuaFile()
-
-ENT.PrintName = "NPC Top-Attack Missile"
-ENT.Author    = ""
-ENT.Category  = ""
-ENT.Base      = "base_entity"
-ENT.Type      = "anim"
-ENT.Spawnable = false
--- entities/entities/sent_npc_trackmissile/shared.lua
+++
@@ -1,8 +0,0 @@
-AddCSLuaFile()
-
-ENT.PrintName = "NPC Tracking Missile"
-ENT.Author    = ""
-ENT.Category  = ""
-ENT.Base      = "base_entity"
-ENT.Type      = "anim"
-ENT.Spawnable = false
